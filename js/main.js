a7d9376baafaef6660a423337062d8eb55864069
RashaElnaggar <rolly_elnaggar@yahoo.com>
2023-03-18 13:31:47 +0200

    design added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const addTask = document.getElementById("addButton");
+const tasksTodo = document.getElementById("toDotasks");
+const el = document.createElement('div');
+el.classList.add("task");
+el.innerHTML="new task"
+el.innerHTML=parseInt()
+addTask.addEventListener('click', addTask);
+function addTask() {
+  tasksTodo.appendChild("el");
+}
