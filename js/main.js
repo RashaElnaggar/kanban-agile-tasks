b2631aea5e0226249d198f1bbe8fcafd09689a03
RashaElnaggar <rolly_elnaggar@yahoo.com>
2023-03-29 13:45:09 +0200

    update

--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ let drag=null;
 
 addTaskbtn.addEventListener('click',addTask);
 function addTask() {
- let name=Taskinput.value;
+ var name=Taskinput.value;
  console.log(name);
  if(name!="") {
 //  tasksTodo.insertAdjacentHTML("beforeend",`<li class="task" draggable="true">${name}</li>`);
 tasksTodo.innerHTML += `<li class="task" draggable="true">${name}</li>`;
- Taskinput.value="";
+   Taskinput.value = "";
+   console.log("new task added");
 }
 
 dragTasks();
@@ -45,8 +46,40 @@ function dragTasks(){
  taskcol.addEventListener('drop',function(){
   taskcol.append(drag);
   this.style.background="#fff";
-  this.style.color="#fff";
+   this.style.color = "#fff";
+   makeJson();
 });
  });
 });
+}
+// document.addEventListener('drop', function(event){
+//   event.preventDefault();
+//   var data = event.dataTransfer.getData("text");
+//   event.target.append(document.getElementById("data"));
+//   localStorage.setItem('section1', JSON.stringify(document.getElementById("toDotasks")));
+//   localStorage.setItem('section2', JSON.stringify(document.getElementById("inProgress")).innerHTML);
+//   localStorage.setItem('section3', JSON.stringify(document.getElementById("completed")));
+// });
+window.onload = restoreJason;
+
+function makeJson() {
+  const sections = document.getElementById("sections");
+  var shapes = sections.querySelectorAll("div[class='section']");
+  var divs = [];
+  console.log(shapes.length);
+  for(var i=0; i<shapes.length; i++){
+    // divs[shapes[i].getAttribute('innerHTML')] = shapes[i].innerHTML;
+    divs.push(shapes[i].innerHTML);
+  }
+  
+  localStorage.setItem("divs", JSON.stringify(divs));
+}
+function restoreJason() {
+  var divs = JSON.parse(localStorage.getItem("divs"));
+  const sections = document.getElementById("sections");
+  var shapes = sections.querySelectorAll("div[class='section']");
+  for(var i = 0; i<shapes.length; i++){
+    shapes[i].innerHTML = divs[i];
+}
+console.log(divs);
 }
